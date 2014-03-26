4c44314ec87995860ce3410acb2abfd9d48de6e5
Administrador <Administrador@lds10.unac.edu.co>
2014-03-26 17:28:02 -0500

    guardando los datos

---
+++ src/Students/Student.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Students;
+
+/**
+ *
+ * @author asis3.desarrollo
+ */
+public class Student {
+    public int StudentIdentificationNumber;
+    public String FirstName;
+    public String LastName;
+    public int CreditsSoFar;
+    public double GPA;
+}
